2514e0e59f47a8e12745ddbcd46461d06e3f29ed
PopeRaj <51174148+PopeRaj@users.noreply.github.com>
2023-04-03 17:23:36 -0400

    phonebook part2b

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const[showAll, setShowAll] = useState(true) 
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber,
+    }
+    if (!persons.filter(e => e.name === newName).length > 0){
+      setPersons(persons.concat(nameObject))
+    }
+    else{
+      window.alert(`${newName} is already added to phonebook`)
+    }
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setShowAll(false)
+    setNewFilter(event.target.value)
+    if (event.target.value == 0){
+      setShowAll(true)
+    }
+  }
+
+
+  const namesToShow = showAll 
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={newFilter} handle = {handleFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm add={addName} name={newName} handleName = {handleNameChange} number={newNumber} handleNumber = {handleNumberChange}/>
+      <h3>Numbers</h3>
+      <Persons namesToShow = {namesToShow} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,13 @@
+import Person from './Person'
+
+const Persons = ({namesToShow}) =>{
+    return(
+    <div>
+    {namesToShow.map(person =>
+    <Person key = {person.name} person = {person}/>
+    )}
+    </div>
+    )
+}
+
+export default Persons
